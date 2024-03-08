c6664405b40b31df2f65c121f07367fb1cacf4b7
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-03-08 22:40:18 +0100

    App components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/notepad/Header";
+import Footer from "./components/notepad/Footer";
+import Note from "./components/notepad/Note";
+import CreateNote from "./components/notepad/CreateNote";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [displayNotePad, setDisplayNotePad] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
+  const displayNote = () => {
+    setDisplayNotePad((prevNote) => !prevNote);
+  };
+  const addNote = (newNote) => {
+    setNotes((prevNote) => {
+      return [...prevNote, newNote];
+    });
+  };
+  const deleteNote = (id) => {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  };
+
   return (
-    <div className="App">
-      
+    <div className={darkMode ? "appDark" : "appLight"}>
+      {displayNotePad ? (
+        <Header
+          displayNote={displayNote}
+          darkMode={darkMode}
+          toggleDarkMode={toggleDarkMode}
+        />
+      ) : (
+        <CreateNote onAdd={addNote} />
+      )}
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
     </div>
   );
 }
