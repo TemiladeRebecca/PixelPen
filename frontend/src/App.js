632be2e5b25c42614323ff9d6ec3736aaebee8e0
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-03-24 20:50:58 +0100

    Added the generateRandomColor function, which generates a random color using the HSLA color model. This function is used to dynamically set background colors in the application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from "./components/Home";
 import About from "./components/About";
@@ -14,8 +14,27 @@ import Footer from "./components/Footer";
 
 function App() {
 
+  const [backgroundColor, setbackgroundColor] = useState();
+
+  const generateRandomColor = () => {
+    const hue = Math.floor(Math.random() * 360);
+    const saturation = Math.floor(Math.random() * 5) + 95; // 95-100%
+    const lightness = Math.floor(Math.random() * 5) + 95; // 95-100%
+    const alpha = Math.floor(Math.random() * 5) + 3; // 3-7%
+    const color = `hsla(${hue}, ${saturation}%, ${lightness}%, 0.${alpha})`;
+    setbackgroundColor(color);
+  };
+
+  useEffect(() => {
+    generateRandomColor();
+  }, []);
+
+  const backgroundStyle = {
+    backgroundColor: backgroundColor,
+  }
+
   return (
-    <div>
+    <div style={backgroundStyle}>
       <BrowserRouter>
       <NavBar />
       <div>
@@ -23,11 +42,12 @@ function App() {
         <Route path="/" element={<Home/>}></Route>
         <Route path="/about" element={<About/>}></Route>
         <Route path="/about/login" element={<Login/>}></Route>
-        <Route path="/about/signup" element={<SignUp/>}></Route>
+        <Route path="/about/notes" element={<Notes/>}></Route>
         <Route path="/about/home" element={<Home/>}></Route>
         <Route path="/about/todo" element={<Todo/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
         <Route path="/signup" element={<SignUp/>}></Route>
+        <Route path="/signup/login" element={<Login/>}></Route>
         <Route path="/signup/home" element={<Home/>}></Route>
         <Route path="/todo" element={<Todo/>}></Route>
         <Route path="/todo/notes" element={<Notes/>}></Route>
