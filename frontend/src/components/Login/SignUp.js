12f3fa30f421ab490bf82d2233fdcfab4ebd9e90
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-03-24 20:07:46 +0100

    Updated the signup page to include input for users to confirm password and route to the login page after signup

--- frontend/src/components/Login/SignUp.js
+++ frontend/src/components/Login/SignUp.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 
 function SignUp () {
 
     return (
-        <div class="row align-items-center g-lg-5 py-5" style={{marginBottom: "20%",}}>
-      <div class="col-lg-6 text-center text-lg-start" style={{margin: "50px"}}>
-        <h1 class="display-4 lh-1 text-body-emphasis mb-3">Vertically centered hero sign-up form</h1>
-        <p class="col-lg-8 fs-5">Below is an example form built entirely with Bootstrap form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
+        <div class="row align-items-center g-lg-2 py-3" style={{margin: "50px 50px 150px 50px"}}>
+      <div class="col-lg-6 text-center text-lg-start" style={{marginTop: "0px"}}>
+        <h1 class="display-4 lh-1 text-body-emphasis mb-3" style={{ fontFamily: 'Arial, serif' }}>Welcome to PixelPen</h1>
+        <p class="col-lg-8 fs-5" style={{marginLeft: "10px"}}>Capture every idea, accomplish every goal. <span style={{marginLeft: "100px"}}>Start with us Today!</span></p>
       </div>
-      <div class="col-md-10 mx-auto col-lg-4">
-        <form class="p-4 p-md-5 border rounded-3 bg-body-tertiary">
+      <div class="col-md-8 mx-auto col-lg-4">
+        <form class="p-4 p-md-5 border rounded-3" style={{backgroundColor: "white"}}>
           <div class="form-floating mb-3">
             <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"></input>
             <label for="floatingInput">Email address</label>
@@ -23,7 +24,7 @@ function SignUp () {
             <input type="password" class="form-control" id="floatingPassword" placeholder="Confirm password"></input>
             <label for="floatingPassword">Confirm Password</label>
           </div>
-          <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
+          <Link class="w-100 btn btn-lg btn-primary" role="button" to="/login">Sign up</Link>
           <small class="text-body-secondary">By clicking Sign up, you agree to the terms of use.</small>
         </form>
       </div>
