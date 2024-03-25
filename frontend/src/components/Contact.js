7ffe604bf0884cd796d0286ccd2168fc7ba04a3e
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-03-26 00:48:17 +0100

    Created a contact page to help users with enquiries and updates

---
+++ frontend/src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Contact() {
+    return (
+        <div>
+            <div className="px-4 pt-5 my-5 text-center border-bottom">
+                <h1 className="display-4 text-body-emphasis" style={{fontFamily: 'Times New Roman, serif' }}>Contact Us</h1>
+            </div>
+        </div>
+    );
+}
+
+
+export default Contact;
