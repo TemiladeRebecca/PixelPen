3ba9c4e28338d4fb21e462e752fa59af260ee2f7
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-03-27 21:41:08 +0100

    Improved on the home page to make it responsive

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from 'react-router-dom';
 import image from "../Images/notepad_image1.jpg";
 import image1 from "../Images/notepad_image2.jpg"
@@ -6,14 +6,26 @@ import image1 from "../Images/notepad_image2.jpg"
 
 
 function Home () {
+
+  const [btnHovered, setBtnHovered] = useState(false)
+  const styles = {
+    "btn:hover": {
+      boxShadow: "8px 8px 1px -5px rgba(0, 0, 0, 0.9)",
+      transition: "transform 0.3s linear",
+      transform: "scale(1.1)",
+    },
+  }
     return (
         <>
         <div className="px-4 pt-5 my-5 text-center border-bottom">
-    <h1 className="display-4 text-body-emphasis" style={{fontFamily: 'Times New Roman, serif' }}>Organize yours thoughts effortlessly</h1>
+    <h1 className="display-4 fw-bold text-body-emphasis" style={{fontFamily: 'Times New Roman, serif' }}>Organize yours thoughts effortlessly</h1>
     <div className="col-lg-6 mx-auto">
       <p className="lead mb-4" style={{fontFamily: "Helvetica, sans-serif"}}>With our intuitive and user-friendly interface, you can quickly create, edit, and manage notes wherever you go, whether you're at home, work, or on the go</p>
       <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-      <Link className="btn btn-outline-primary mx-2 text-black" to="./login" role="button">Start for free</Link>
+      <Link className="btn btn-primary btn-lg px-4 me-md-2 text-black" to="./login" role="button"
+       style={{...(btnHovered && styles["btn:hover"]), marginTop: "20px"}}
+       onMouseEnter={() => setBtnHovered(true)}
+        onMouseLeave={() => setBtnHovered(false)}>Start for free</Link>
       </div>
     </div>
     <div className="overflow-hidden">
@@ -23,18 +35,18 @@ function Home () {
     </div>
   </div>
   <div className="container px-4 py-5" id="featured-3">
-    <div className="row g-4 py-5 row-cols-1 row-cols-lg-3" style={{marginTop: "-50px"}}>
+    <div className="row g-4 py-5 row-cols-1 row-cols-lg-3" style={{marginTop: "-70px"}}>
       <div className="feature col">
-        <h3 className="fs-2 text-body-emphasis">Featured title</h3>
-        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+        <h3 className="fs-2 text-body-emphasis fw-bold" style={{fontFamily: 'Times New Roman, serif'}}>Stay organized</h3>
+        <p style={{fontFamily: "Helvetica, sans-serif"}}>Remember everything by getting what you need, and when you need it, with powerful and flexible search capabilities.</p>
       </div>
       <div className="feature col">
-        <h3 className="fs-2 text-body-emphasis">Featured title</h3>
-        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+        <h3 className="fs-2 text-body-emphasis fw-bold" style={{fontFamily: 'Times New Roman, serif'}}>Prioritize tasks</h3>
+        <p style={{fontFamily: "Helvetica, sans-serif"}}>Turn to-dos into done by efficiently managing tasks, schedules, ideas and appointments easily and effectively.</p>
       </div>
       <div className="feature col">
-        <h3 className="fs-2 text-body-emphasis">Featured title</h3>
-        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+        <h3 className="fs-2 text-body-emphasis fw-bold" style={{fontFamily: 'Times New Roman, serif'}}>Work efficiently</h3>
+        <p style={{fontFamily: "Helvetica, sans-serif"}}>Keep important information handy and together. Update, edit, add notes and tasks from anywhere you are.</p>
       </div>
     </div>
   </div>
@@ -45,7 +57,7 @@ function Home () {
         <div className="container">
           <div className="carousel-caption text-start" style={{marginBottom: "70px"}}>
             <h1>Unlock your creativity with every note.</h1>
-            <p className="opacity-75">Ideas are worth saving.</p>
+            <p className="opacity-75">Ideas are worth saving</p>
           </div>
         </div>
       </div>
@@ -75,7 +87,7 @@ function Home () {
         <img src={image1} className="d-block mx-lg-auto img-fluid" alt="PIC" width="700" height="500" loading="lazy" />
       </div>
       <div className="col-lg-6">
-        <h1 className="display-5 text-body-emphasis lh-1 mb-3" style={{fontFamily: 'Times New Roman, serif', marginTop: "-50px" }}>Hit every deadline</h1>
+        <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3" style={{fontFamily: 'Times New Roman, serif', marginTop: "-50px" }}>Hit every deadline</h1>
         <p className="lead" style={{fontFamily: "Helvetica, sans-serif", marginTop: "-10px"}}>Nothing important needs to be forgotten with our todo list feature to manage tasks, important schedules and stay productive</p>
       </div>
     </div>
