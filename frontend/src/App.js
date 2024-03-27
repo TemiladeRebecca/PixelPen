7993cd869c738500b1f9e87d78543b99fc4278f3
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-03-27 23:58:08 +0100

    Modified an NoteCard import and route in the App page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,12 @@ import React, {useState, useEffect} from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from "./components/Home";
 import About from "./components/About";
+import Contact from "./components/Contact"
 import Login from "./components/Login/Login";
 import SignUp from "./components/Login/SignUp";
 import NavBar from "./components/NavBar";
-import Todo from "./components/Todo List/Todo";
-import Notes from "./components/Notes/Notes";
+import Todo from "./components/Todolist/Todo";
+import Note from "./components/Notes/NotesCard";
 import Footer from "./components/Footer";
 
 
@@ -42,16 +43,17 @@ function App() {
         <Route path="/" element={<Home/>}></Route>
         <Route path="/about" element={<About/>}></Route>
         <Route path="/about/login" element={<Login/>}></Route>
-        <Route path="/about/notes" element={<Notes/>}></Route>
+        <Route path="/about/notes" element={<Note/>}></Route>
         <Route path="/about/home" element={<Home/>}></Route>
         <Route path="/about/todo" element={<Todo/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
+        <Route path="/login/signup" element={<SignUp/>}></Route>
         <Route path="/signup" element={<SignUp/>}></Route>
         <Route path="/signup/login" element={<Login/>}></Route>
-        <Route path="/signup/home" element={<Home/>}></Route>
         <Route path="/todo" element={<Todo/>}></Route>
-        <Route path="/todo/notes" element={<Notes/>}></Route>
-        <Route path="/notes" element={<Notes/>}></Route>
+        <Route path="/todo/notes" element={<Note/>}></Route>
+        <Route path="/notes" element={<Note/>}></Route>
         <Route path="/notes/todo" element={<Todo/>}></Route>
       </Routes>
       <Footer />
