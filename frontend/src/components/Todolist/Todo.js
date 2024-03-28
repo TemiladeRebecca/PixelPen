4de73aac6f8b0826e2ac62d5230db7c4174e5074
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-03-28 15:47:00 +0100

    Added bell icon to the tasks  side bar for notification alert and set priority 4 as default color if the user doesn't select a priority

--- frontend/src/components/Todolist/Todo.js
+++ frontend/src/components/Todolist/Todo.js
@@ -95,7 +95,8 @@ function addTodo(event) {
  function handleDelete(id) {
   setTaskList(prevValue => {
     return prevValue.filter((title, index) => {
-      return index !== id
+      // return all tasks with id not equal to the selected task
+      return index !== id 
     });
   });
 }
@@ -125,18 +126,23 @@ function addTodo(event) {
  
     return (
       <div style={{display: "flex", marginBottom: "20%"}}>
-        <div className="d-flex flex-column flex-shrink-0 p-3 bg-body-white" style={{width: "230px", marginRight: "10px"}}>
-          <div className="dropdown">
+        <div className="d-flex flex-column flex-shrink-1 p-3 bg-body-white" style={{width: "300px", marginRight: "10px"}}>
+          <div className="dropdown" style={{display: "flex", alignItems: "center"}}>
       <a href="p" className="d-flex align-items-center link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-        <span>temilade.rebecca20</span><i class="bi bi-bell"></i>
-      </a>
+        <span>temilade.rebecca20</span>
+      </a> 
       <ul className="dropdown-menu text-small shadow">
         <li><button className="dropdown-item" onClick={handleTask}>New task...</button></li>
         <li><a className="dropdown-item" href="p">Settings</a></li>
         <li><a className="dropdown-item" href="p">Profile</a></li>
         <li><hr className="dropdown-divider"></hr></li>
         <li><a className="dropdown-item" href="p">Sign out</a></li>
-      </ul>
+      </ul> 
+        <div style={{marginLeft: "10px"}}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
+          <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
+        </svg>
+        </div>     
     </div>
     <ul className="nav nav-pills flex-column mb-auto">
       <li className="nav-item">
@@ -168,7 +174,7 @@ function addTodo(event) {
     </ul>
   </div>
   {showTask &&
-    <div className="container" style={styles.container}>
+    <div className="container " style={{...styles.container}}>
       <form className="form" style={styles.form}>
         <input
           onChange={handleChange}
@@ -194,7 +200,7 @@ function addTodo(event) {
               <li><button className="dropdown-item" onClick={handlePriority1}><button className="btn btn-sm me-2" style={{backgroundColor: "#FF0000"}}></button>Priority 1</button></li>
               <li><button className="dropdown-item" onClick={handlePriority2}><button className="btn btn-sm me-2" style={{backgroundColor: "#FFFF33"}}></button>Priority 2</button></li>
               <li><button className="dropdown-item" onClick={handlePriority3}><button className="btn btn-sm me-2" style={{backgroundColor: "#0000CC"}}></button>Priority 3</button></li>
-              <li><button className="dropdown-item" onClick={handlePriority4}><button className="btn btn-sm me-2" style={{backgroundColor: "#000000"}}></button>Priority 4</button></li>
+              <li><button className="dropdown-item" onClick={handlePriority4}><button className="btn btn-sm me-2" style={{backgroundColor: "#000000"}}></button>Priority 4 <span>&#10003;</span></button></li>
             </ul>
         </div>
         <div style={{margin: "-30px 0 0 390px"}}>
@@ -205,17 +211,21 @@ function addTodo(event) {
     </div>}
 
     {showTaskList &&
-    <div style={{ width: "100%", height: "100vh"}}>
-    <h5 style={{marginLeft: "50px", fontFamily: 'Times New Roman, serif'}}>Task List</h5>
+    <div style={{ width: "100%", height: "100vh", marginLeft: "100px"}}>
+    <h5 style={{fontFamily: 'Times New Roman, serif', justifyContent: "content"}}>Task List</h5>
       {taskList.map((title, index) => (
-        <div key={index} id={index} style={{...styles.form, width: "300px", height: "200px"}}>
-          <div><button className="btn btn-sm me-2" style={{backgroundColor: title.tagColor}}></button>{title.task}</div>
-          <div>{title.description}</div>
-          <div style={{margin: "80px 0 0 200px"}}><button className="btn btn-danger btn-sm mx-2 text-black btn-block mt-3" onClick={() => handleDelete(index)}>Delete</button></div>
+        <div class="card" key={index} id={index}style={{width: "18rem"}}>
+        <div class="card-body">
+          <h6 class="card-subtitle mb-2 text-body-secondary"><button className="btn btn-sm me-2" style={{backgroundColor: title.tagColor}}></button> {title.task}</h6>
+          <p class="card-text">{title.description}</p>
+          <div style={{marginLeft: "180px"}}>
+          <button className="btn btn-danger btn-sm mx-2 text-black btn-block mt-3" onClick={() => handleDelete(index)}>Delete</button>
+          </div>
         </div>
+      </div>
       ))}
   </div> }
-     
+  
   </div>
 
     );
