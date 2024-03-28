78939ec40a9cca3bc4f6bd3c6f124d676d190128
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-03-28 15:43:49 +0100

    Added bell icon to the side bar  for notifications alert

--- frontend/src/components/Notes/NotesCard.js
+++ frontend/src/components/Notes/NotesCard.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import {Link} from "react-router-dom";
 
 export default function Note() {
-    const [showNote, setshowNote] = useState(false);
+    const [showNote, setShowNote] = useState(false);
     const [color, setColor] = useState(null);
     const [textColor, setTextColor] = useState('#808080');
     const [showTag, setShowTag] = useState("Select a tag");
@@ -12,6 +12,8 @@ export default function Note() {
       title: "",
       description: ""
     });
+    const [createNoteItem, setCreateNoteItem] = useState([]);
+    const [showNoteList, setShowNoteList] = useState(false);
 
     useEffect(() => {
         const colors = [
@@ -78,8 +80,14 @@ export default function Note() {
       },
     }
 
+    const date = new Date();
+    const options = { weekday: 'short', day: 'numeric', month: 'short' };
+    const newDate = date.toLocaleDateString('en-NG', options);
+
+
     function handleNotes () {
-        setshowNote((prevValue) => !prevValue);
+        setShowNote((prevValue) => !prevValue);
+        setShowNoteList(false);
     }
 
     function onChange (event) {
@@ -109,61 +117,103 @@ export default function Note() {
       setShowInputText((prevValue) => !prevValue);
       setInputValue({input: ""});
     }
+
+    function addNote(event) {
+      // Create a new task object with task, description, tagColor, and priority properties
+      const newTask = {
+        title: createNewNote.title,
+        description: createNewNote.description,
+        tag: showTag,
+        noteDate: newDate,
+      };
     
+      // Update the task list with the new task object
+      setCreateNoteItem((prevValue) => [...prevValue, newTask]);
+    
+      // Clear the input fields
+      setCreateNewNote({
+        title: "",
+        description: "",
+      });
+    
+      // Reset the priority and tag color
+      setShowTag("Select a tag");
+    
+      // Prevent the default form submission behavior
+      event.preventDefault();
+    }
+
+    function handleShowNoteList () {
+      setShowNoteList((prevValue) => !prevValue);
+    }
+
+    function handleDeleteNote(id) {
+      setCreateNoteItem(prevValue => {
+        return prevValue.filter((note, index) => {
+          // return all notes with id not equal to the selected task
+          return index !== id 
+        });
+      });
+    }
+
     return (
         <div style={{ display: "flex", marginBottom: "20%"}}>
           <div className="d-flex flex-column flex-shrink-0 p-3" style={{width: "230px", backgroundColor: "white"}}>
-          <div className="dropdown">
-      <a href="p" className="d-flex align-items-center link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-        <span>temilade.rebecca20</span><i class="bi bi-bell"></i>
-      </a>
-      <ul className="dropdown-menu text-small shadow">
-        <li><button className="dropdown-item" onClick={handleNotes}>New note...</button></li>
-        <li><a className="dropdown-item" href="p">Settings</a></li>
-        <li><a className="dropdown-item" href="p">Profile</a></li>
-        <li><hr className="dropdown-divider"></hr></li>
-        <li><a className="dropdown-item" href="p">Sign out</a></li>
-      </ul>
-    </div>
-    <ul className="nav nav-pills flex-column mb-auto">
-      <li className="nav-item">
-        <div style={{marginLeft: "5px"}}>
-        <button className="btn text-black btn-block mt-3" onClick={handleNotes}>Add Note</button>
+          <div className="dropdown" style={{display: "flex", alignItems: "center"}}>
+          <a href="p" className="d-flex align-items-center link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            <span>temilade.rebecca20</span><i class="bi bi-bell"></i>
+          </a>
+          <ul className="dropdown-menu text-small shadow">
+            <li><button className="dropdown-item" onClick={handleNotes}>New note...</button></li>
+            <li><a className="dropdown-item" href="p">Settings</a></li>
+            <li><a className="dropdown-item" href="p">Profile</a></li>
+            <li><hr className="dropdown-divider"></hr></li>
+            <li><a className="dropdown-item" href="p">Sign out</a></li>
+          </ul>
+          <div style={{marginLeft: "10px"}}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
+          <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
+        </svg>
         </div>
-      </li>
-      <li>
-        <a href="p" className="nav-link link-body-emphasis">
-          
-          Search
-        </a>
-      </li>
-      <li>
-        <a href="p" className="nav-link link-body-emphasis">
-          Today
-        </a>
-      </li>
-      <li>
-      <div style={{margin: "-10px 0 0 2px"}}>
-      <button className="btn text-black btn-block mt-3" >Note List</button>
-      </div>
-      </li>
-      <li>
-      <div style={{margin: "-10px 0 0 2px"}}>
-      <Link className="btn text-black btn-block mt-3" to="./todo" role="button">Create Tasks</Link>
-      </div>
-      </li>
-    </ul>
-  </div>  
+        </div>
+          <ul className="nav nav-pills flex-column mb-auto">
+            <li className="nav-item">
+            <div style={{marginLeft: "5px"}}>
+              <button className="btn text-black btn-block mt-3" onClick={handleNotes}>Add Note</button>
+            </div>
+            </li>
+            <li>
+              <a href="p" className="nav-link link-body-emphasis"> 
+                Search
+              </a>
+            </li>
+            <li>
+              <a href="p" className="nav-link link-body-emphasis">
+               Today
+              </a>
+            </li>
+            <li>
+              <div style={{margin: "-10px 0 0 2px"}}>
+              <button className="btn text-black btn-block mt-3" onClick={() => handleShowNoteList()}>Note List</button>
+              </div>
+            </li>
+            <li>
+              <div style={{margin: "-10px 0 0 2px"}}>
+              <Link className="btn text-black btn-block mt-3" to="./todo" role="button">Create Tasks</Link>
+             </div>
+            </li>
+          </ul>
+        </div>  
         {showNote &&
-        <div className="col-md-4 mb-4">
+          <div className="col-md-4 mb-4">
             <div className="card shadow h-100" style={{ backgroundColor: color, color: textColor, marginLeft: "20px", width: "500px" }}>
-                <div className="card-body d-flex flex-column justify-content-between">
-                    <div className="d-flex justify-content-left">
-                        <p className="card-text mb-1 mt-2"></p>
-                    </div>
-                    <div>
-                    <form className="form" style={{backgroundColor: color}}>
-                      <input
+              <div className="card-body d-flex flex-column justify-content-between">
+                <div className="d-flex justify-content-left">
+                  <p className="card-text mb-1 mt-2" style={{marginLeft: "350px"}}>{newDate}</p>
+                  </div>
+                <div>
+                  <form className="form" style={{backgroundColor: color}}>
+                    <input
                       onChange={onChange}
                       name="title"
                       placeholder="Title..."
@@ -176,41 +226,53 @@ export default function Note() {
                         value={createNewNote.description}
                         rows="10"
                         style={{...styles.textarea, backgroundColor: color}}/>
-                      </form>
-                    </div>
-                    <div>
-                    <hr className="mt-2 mb-3"/>
-                    <div className="dropdown">
-                      <button className="d-flex align-items-center text-decoration-none dropdown-toggle btn text-black btn-block mt-3" data-bs-toggle="dropdown" aria-expanded="false">
-                        <span>{showTag}</span>
-                      </button>
-                        <ul className="dropdown-menu text-small shadow">
-                          <li><button className="dropdown-item" onClick={handleTagShopping}>Shopping</button></li>
-                          <li><button className="dropdown-item" onClick={handleTagWork}>Work</button></li>
-                          <li><button className="dropdown-item" onClick={handleTagPersonal}>Personal</button></li>
-                          <li>
-                          <button className="dropdown-item" onClick={handleShowInputText}>Others...</button>
-                          {showInputText && (
-                          <li>
+                  </form>
+                  </div>
+                <div>
+                  <hr className="mt-2 mb-3"/>
+                  <div className="dropdown">
+                    <button className="d-flex align-items-center text-decoration-none dropdown-toggle btn text-black btn-block mt-3" data-bs-toggle="dropdown" aria-expanded="false">
+                      <span>{showTag}</span>
+                    </button>
+                      <ul className="dropdown-menu text-small shadow">
+                        <li><button className="dropdown-item" onClick={handleTagShopping}>Shopping</button></li>
+                        <li><button className="dropdown-item" onClick={handleTagWork}>Work</button></li>
+                        <li><button className="dropdown-item" onClick={handleTagPersonal}>Personal</button></li>
+                        <li>
+                        <button className="dropdown-item" onClick={handleShowInputText}>Others...</button>
+                        {showInputText && (
+                        <li>
                           <input onChange={inputChange} name="input" value={inputValue.input}/>
-                          </li>
-                          )}
+                        </li>)}
                           </li>
                         </ul>
                     </div>
-
-                        <div className="d-flex justify-content-end">
-                            <button className="btn btn-sm btn-outline-danger me-2" onClick={handleNotes}>
-                                <i className="fas fa-trash-alt"></i>
-                            </button>
-                            <button className="btn btn-sm btn-outline-primary">
-                                <i className="fas fa-edit"></i>
-                            </button>
-                        </div>
-                    </div>
+                  <div className="d-flex justify-content-end" style={{marginBottom: "10px"}}>
+                    <button className="btn btn-success btn-sm mx-2 text-black btn-block mt-3" onClick={addNote}>Add Note</button>
+                    <button className="btn btn-danger btn-sm mx-2 text-black btn-block mt-3" onClick={handleNotes}>Cancel</button>
                 </div>
+              </div>
             </div>
-        </div>}
-        </div>
-    );
+          </div>
+        </div>}        
+      {showNoteList &&
+      <div style={{marginLeft: "50px"}}>
+        <h5 style={{fontFamily: 'Times New Roman, serif', justifyContent: "content", marginLeft: "100px"}}>Note List</h5>
+          {createNoteItem.map((note, index) => (
+            <div class="card" key={index} style={{width: "23rem"}}>
+            <div class="card-body" key={index}>
+              <h5 class="card-title">{note.noteDate}</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary">{note.title}</h6>
+              <p class="card-text">{note.description}</p>
+              <p className="card-text"><small><strong>Tag:</strong> {note.tag}</small></p>
+              <div className="d-flex justify-content-end">
+                <button className="btn btn-sm btn-outline-danger me-2" onClick={() => handleDeleteNote(index)}></button>
+                <button className="btn btn-sm btn-outline-primary"></button>
+              </div>   
+            </div>
+          </div>
+        ))}
+    </div>}    
+  </div>
+  );
 }
